7b1c47f816fbeac3361d3c2ab6cfe515a20905d3
Daniel-Heinen <254339149+Daniel-Heinen@users.noreply.github.com>
2022-01-06 17:47:39 +0000

    feat: add core functionality

---
+++ config/settings.py
@@ -0,0 +1,7 @@
+"""Application settings"""
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://localhost/db")
+SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-key")
+DEBUG = os.getenv("DEBUG", "True") == "True"
+# Modified 2025-08-23
