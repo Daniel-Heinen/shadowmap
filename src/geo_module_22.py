4a196add35b4ef78fb15f22426a0974984f0877d
Daniel-Heinen <254339149+Daniel-Heinen@users.noreply.github.com>
2022-03-15 09:27:01 +0000

    feat: add API endpoints

---
+++ src/geo_module_22.py
@@ -0,0 +1,13 @@
+"""Geolocation module 22 for shadowmap"""
+import numpy as np
+import torch
+from typing import List, Dict
+
+class LocationAnalyzer22:
+    def __init__(self):
+        self.model = torch.nn.Linear(100, 2)
+    
+    async def analyze(self, data: np.ndarray) -> Dict:
+        result = self.model(torch.tensor(data))
+        return {"lat": float(result[0]), "lng": float(result[1])}
+# Modified 2024-06-27
