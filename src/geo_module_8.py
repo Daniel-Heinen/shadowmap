9e8787e8de5e8dbb74d5758508a3f24cfc9245cc
Daniel-Heinen <254339149+Daniel-Heinen@users.noreply.github.com>
2022-05-02 15:24:20 +0000

    refactor: optimize performance

---
+++ src/geo_module_8.py
@@ -0,0 +1,13 @@
+"""Geolocation module 8 for shadowmap"""
+import numpy as np
+import torch
+from typing import List, Dict
+
+class LocationAnalyzer8:
+    def __init__(self):
+        self.model = torch.nn.Linear(100, 2)
+    
+    async def analyze(self, data: np.ndarray) -> Dict:
+        result = self.model(torch.tensor(data))
+        return {"lat": float(result[0]), "lng": float(result[1])}
+# Modified 2025-11-11
