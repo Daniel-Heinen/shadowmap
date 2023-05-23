d810ab522cf8e2bc018ce5d648402c7cefc8124d
Daniel-Heinen <254339149+Daniel-Heinen@users.noreply.github.com>
2023-05-23 12:27:29 +0000

    perf: improve response time

--- src/geo_module_32.py
+++ src/geo_module_32.py
@@ -11,3 +11,4 @@ class LocationAnalyzer32:
         result = self.model(torch.tensor(data))
         return {"lat": float(result[0]), "lng": float(result[1])}
 # Modified 2025-08-25
+# Modified 2023-05-23
