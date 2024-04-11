c8fff7b1bdcb46724f83dbdfd5d4758f7973b2d5
Daniel-Heinen <254339149+Daniel-Heinen@users.noreply.github.com>
2024-04-11 09:24:41 +0000

    feat: add API endpoints

--- src/geo_module_7.py
+++ src/geo_module_7.py
@@ -11,3 +11,4 @@ class LocationAnalyzer7:
         result = self.model(torch.tensor(data))
         return {"lat": float(result[0]), "lng": float(result[1])}
 # Modified 2023-08-15
+# Modified 2024-04-11
